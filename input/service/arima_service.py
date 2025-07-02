16460da6e82217f6a2acd7ba559dc38e4ab12c0b
soolyn <soolyn4867@gmail.com>
2025-07-02 11:37:01 +0900

    fix : 수요예측 구현

--- input/service/arima_service.py
+++ input/service/arima_service.py
@@ -2,24 +2,12 @@ import os
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 
-def arima_function(file_path=None, steps=12):
-    if file_path is None:
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        file_path = os.path.join(BASE_DIR, "data", "monthly_sales_volume.xlsx")
-
-    ext = os.path.splitext(file_path)[-1].lower()
-    if ext in [".xls", ".xlsx"]:
-        data = pd.read_excel(file_path, dtype={"시점": "str"})
-    elif ext == ".csv":
-        data = pd.read_csv(file_path, dtype={"시점": "str"})
-    else:
-        raise ValueError("지원하지 않는 파일 형식입니다.")
-
+def arima_function(data, steps=12):
     data.columns = ["date", "sales_volume"]
     data["date"] = pd.to_datetime(data["date"], format="%Y.%m")
     data = data.set_index("date")
     ts = data["sales_volume"].astype(float)
-    model = ARIMA(ts, order=(1,1,1))
+    model = ARIMA(ts, order=(1,2,1))
     fit = model.fit()
     forecast = fit.forecast(steps=steps)
     last_date = ts.index[-1]
@@ -30,8 +18,7 @@ def arima_function(file_path=None, steps=12):
     df_pred = pd.DataFrame({'date': forecast.index.astype(str), 'value': forecast.values, 'type': 'forecast'})
 
     result = pd.concat([df_actual, df_pred], ignore_index=True)
-    # NaN → None 변환
     result = result.replace({pd.NA: None, pd.NaT: None, float('nan'): None})
     result = result.where(pd.notnull(result), None)
+    return result.to_dict(orient="records")
 
-    return result
